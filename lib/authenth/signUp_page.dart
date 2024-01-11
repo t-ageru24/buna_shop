8c1255f7528fc8a0b91da7f8e53a8dd03616ca9b
t-ageru24 <tadesseageru13@gmail.com>
2024-01-11 15:07:42 +0300

    update

--- lib/authenth/signUp_page.dart
+++ lib/authenth/signUp_page.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-
 import '../pages/home_page.dart';
 import '../authenth/login_page.dart';
 
@@ -8,6 +7,11 @@ class SignUpPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
+    List images = [
+      "google0.png",
+      "apple0.png",
+    ];
 
     double w = MediaQuery.of(context).size.width;
     double h = MediaQuery.of(context).size.height;
@@ -33,7 +37,7 @@ class SignUpPage extends StatelessWidget {
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  SizedBox(height: 50,),
+                  SizedBox(height: 30,),
                   Container(
                     decoration: BoxDecoration(
                         color: Colors.white,
@@ -125,7 +129,7 @@ class SignUpPage extends StatelessWidget {
                     ),
                     child: TextField(
                       decoration: InputDecoration(
-                          hintText: "Re-write your Password!",
+                          hintText: "Re-Enter your Password!",
                           prefixIcon: Icon(Icons.password_outlined, color: Colors.brown,),
                           focusedBorder: OutlineInputBorder(
                               borderRadius: BorderRadius.circular(30),
@@ -165,7 +169,7 @@ class SignUpPage extends StatelessWidget {
               ),
             ),
 
-            SizedBox(height: 50,),
+            SizedBox(height: 40,),
             Container(
               height: 50,
               width: 250,
@@ -182,25 +186,39 @@ class SignUpPage extends StatelessWidget {
                 ),
               ),
             ),
-            SizedBox(height: 50,),
+            SizedBox(height: 20,),
             RichText(
                 text: TextSpan(
-                    text: "Don\'t have an account?",
+                    text: "OR sign up with",
                     style: TextStyle(
                         color: Colors.brown[300],
                         fontSize: 20
                     ),
-                    children: [
-                      TextSpan(
-                        text: " Create",
-                        style: TextStyle(
-                            color: Colors.brown,
-                            fontSize: 20,
-                            fontWeight: FontWeight.bold
+
+                )
+            ),
+            SizedBox(height: 10,),
+            Wrap(
+
+              children: List<Widget>.generate(
+                2,
+                  (index){
+                  return Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: CircleAvatar(
+                      radius: 30,
+                      backgroundColor: Colors.brown,
+                      child: CircleAvatar(
+                        radius: 28,
+                        backgroundImage: AssetImage(
+                          "lib/images/"+images[index],
+                    
                         ),
                       ),
-                    ]
-                )
+                    ),
+                  );
+                  }
+              ),
             )
           ],
         ),
